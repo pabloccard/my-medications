3507bbd3a1997263ad1c62d5d315fcad0fcbda34
Pablo Castilho Cardoso <pabloccard@gmail.com>
2023-02-03 08:32:06 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
+import { defaultTheme } from '../styles/themes/default'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from '@/styles/GlobalStyles'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  black: '#000',
+  white: '#FFF',
+
+  'gray-100': '#F2F2F2',
+  'gray-200': '#D9D9D9',
+  'gray-300': '#808080',
+  'gray-400': '#333333',
+  'gray-500': '#262626',
+  'gray-600': '#1A1A1A',
+  'gray-700': '#0D0D0D',
+
+  purple: '#8284FA',
+  'purple-dark': '#5E60CE',
+
+  'green-500': '#00B37E',
+  'green-600': '#00875F',
+
+  danger: '#E25858',
+} as const
